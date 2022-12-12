172032e189f7d4a8045128bdab18d02be85258ba
Vladislawv <vladusupov2810@gmail.com>
2022-12-12 15:24:02 +0200

    feat - Configured Swagger.

--- Test.Api/Program.cs
+++ Test.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using Test.Services.Areas.Characters;
 using Test.Services.Areas.Characters.Dto;
 using TestApi.Middlewares;
@@ -10,7 +12,29 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddAutoMapper(typeof(IEntityDto));
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Test API",
+        Description = "An ASP.NET Core Web API",
+        TermsOfService = new Uri("https://localhost:5000/terms"),
+        Contact = new OpenApiContact()
+        {
+            Name = "Example contact",
+            Url = new Uri("https://localhost:5000/contact")
+        },
+        License = new OpenApiLicense()
+        {
+            Name = "Example license",
+            Url = new Uri("https://localhost:5000/license")
+        }
+    });
+
+    var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+});
 
 builder.Services.AddTransient<ICharacterService, CharacterService>();
 
@@ -18,8 +42,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options => options.SerializeAsV2 = true);
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"));
 }
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
